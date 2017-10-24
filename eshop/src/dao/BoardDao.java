5e561ecb39d98773eaa062f7992eb2a7eaf9bc31
kimjaein <jaein07227@gmail.com>
2017-10-24 16:25:36 +0900

    dd
    dd

--- eshop/src/dao/BoardDao.java
+++ eshop/src/dao/BoardDao.java
@@ -1,5 +1,48 @@
 package dao;
 
-public class BoardDao {
+import java.sql.*;
 
+import vo.Article;
+
+public class BoardDao {
+	private Connection con;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	private static BoardDao instance = new BoardDao();
+	public static BoardDao getInstance() {
+		return instance;
+	}
+	private BoardDao(){
+		
+	}
+	
+	public int insert(Article article) {
+		con=DBUtil.makeConnection();
+		
+		String sql="INSERT INTO BOARD(BOARD_NAME,TITLE,WRITER,READ_COUNT,WRITE_DATE,CONTENTS,LIST,DEPTH)"
+				+ " VALUES(?,?,?,?,?,?,?,?)" ;
+		int result=0;
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1,article.getBoardName());
+			pstmt.setString(2, article.getTitle());
+			pstmt.setString(3, article.getWriter());
+			pstmt.setInt(4, article.getReadCount());
+			pstmt.setTimestamp(5, new Timestamp(article.getWriteTime().getTime()));
+			pstmt.setString(6, article.getContents());
+			pstmt.setInt(7, article.getList());
+			pstmt.setString(8, article.getDepth());
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			DBUtil.closeCon(con);
+			DBUtil.closePstmt(pstmt);
+		}
+		return result;
+	}
 }
--- eshop/src/servlet/BoardServlet.java
+++ eshop/src/servlet/BoardServlet.java
@@ -1,5 +1,42 @@
 package servlet;
 
-public class BoardServlet {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.BoardService;
+import vo.Article;
+
+@WebServlet("/board")
+public class BoardServlet extends HttpServlet{
+	private BoardService service = BoardService.getInstance();
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("board");
+			request.setCharacterEncoding("euc-kr");
+			String task=request.getParameter("task");
+			
+			if(task.equals("write")) {
+				Article article = new Article();
+				article.setBoardName(request.getParameter("type"));
+				article.setTitle(request.getParameter("title"));
+				article.setWriter(request.getParameter("writer"));
+				article.setContents(request.getParameter("contents"));
+				boolean reault =service.writeArticle(article);
+				
+			}
+	
+	}
 
 }
